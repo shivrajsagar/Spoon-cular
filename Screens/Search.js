5a83703653742f675a7a7b7de62c6204aa35892f
shivraj sagar <shivsagar.sagar294@gmail.com>
2020-12-25 14:04:50 +0530

    spoon

---
+++ Screens/Search.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+class Search extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Search</Text>
+      </View>
+    );
+  }
+}
+export default Search;
